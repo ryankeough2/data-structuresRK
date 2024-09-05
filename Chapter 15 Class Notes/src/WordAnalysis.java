9b1f516723941c872d35ce2081bd8f024015be93
Ryan Keough <rakeough@stu.naperville203.org>
2024-09-05 14:00:02 -0500

    fdw

--- Chapter 15 Class Notes/src/WordAnalysis.java
+++ Chapter 15 Class Notes/src/WordAnalysis.java
@@ -1,9 +1,5 @@
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Scanner;
-import java.util.Set;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.util.*;
 
 /**
  * This program checks which words in a file are not present in a dictionary.
@@ -13,6 +9,14 @@ public class WordAnalysis
     public static void main(String[] args)
         throws FileNotFoundException
     {
+        // Read the dictonary and novel file
+        Set<String> dWords = readWords("Chapter 15 Class Notes/src/words");
+        Set<String> novWords = readWords("Chapter 15 Class Notes/src/war-and-peace.txt");
+        for(String word: novWords){
+            if(!dWords.contains(word)){
+                System.out.println(word);
+            }
+        }
     }
 
     /**
@@ -25,6 +29,18 @@ public class WordAnalysis
     public static Set<String> readWords(String filename)
         throws FileNotFoundException
     {
-        return null;
+        //use a hast set (not tree) because order dont mattet
+        Set<String> words = new HashSet<>();
+        Scanner in = new Scanner(new File(filename), "UTF-8");
+
+        //determine the current working directory
+        //System.out.println(System.getProperty("user.dir"));
+
+        //use any character that's mot a letter
+        in.useDelimiter("[^a-zA-Z]+");
+        while(in.hasNext()){
+            words.add(in.next().toLowerCase());
+        }//duplicates are ignored
+        return words;
     }
 }
